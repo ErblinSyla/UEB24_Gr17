558a83cba1fde1cc9bef137090cd2aab70bf3dbd
Lis Spahija <lispahija@gmail.com>
2025-01-04 13:37:22 +0100

    Added apply for courses modal

---
+++ ourCourses.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("applyModal");
+    const applyButton = document.getElementById("applyButton");
+    const closeModal = document.getElementById("closeModal");
+  
+    applyButton.onclick = function () {
+      modal.style.display = "block";
+    };
+  
+    closeModal.onclick = function () {
+      modal.style.display = "none";
+    };
+  
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  
+    const applyForm = document.getElementById("applyForm");
+    applyForm.onsubmit = function (event) {
+      event.preventDefault();
+      alert("Application submitted!");
+      modal.style.display = "none";
+    };
+  });
